c513dbb976cfbbd355af31b19a2a83ebace15f45
Maximilian Hutchison <mahutchi@c4r1s3.wethinkcode.co.za>
2020-07-09 09:56:37 +0200

    ex00 done

---
+++ ex00/ft_print_alphabet.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+void ft_putchar(char letter){
+	write(1, &letter, 1);
+}
+
+void ft_print_alphabet(void){
+   char letter;
+   letter = 'a';
+
+   while(letter <= 'z' ) {
+	  ft_putchar(letter);
+	  letter++;
+	}
+}   
+
+int main(void){
+	ft_print_alphabet();
+}
+
+
