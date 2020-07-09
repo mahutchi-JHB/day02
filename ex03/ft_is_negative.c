42dc3440cdc81edc07182fec88d369205490b0f8
Maximilian Hutchison <mahutchi@c4r1s3.wethinkcode.co.za>
2020-07-09 15:55:19 +0200

    ex03 complete

---
+++ ex03/ft_is_negative.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+void ft_putchar(int isnegresult){
+	write(1, &isnegresult, 1);
+}
+//int n = '1';
+void ft_is_negative(int n) { 
+	if (n < '0') { 
+		int isnegresult = 'N';
+		ft_putchar(isnegresult); 
+	} else{
+   		int isnegresult = 'P';	
+		ft_putchar(isnegresult); 
+	} 
+}
+
+int main(void){
+	int n = -1;
+	ft_is_negative(n);
+}
+
