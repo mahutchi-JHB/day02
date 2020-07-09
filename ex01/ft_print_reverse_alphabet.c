582e7f4e15bfc3a9f7b9fecfe065da9ed6e21eaf
Maximilian Hutchison <mahutchi@c4r1s3.wethinkcode.co.za>
2020-07-09 08:08:44 +0200

    ex01 done

---
+++ ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void ft_print_reverse_alphabet(void) {
+   char letter;
+
+   for(letter  = 'a' ; letter <= 'z' ; letter++) {
+      putchar(letter);
+   }
+   
+   return(0);
+}
