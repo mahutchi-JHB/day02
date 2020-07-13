6a68cf49456f758821716a1a060249ee7f5d15e4
Maximilian Hutchison <mahutchi@c4r1s3.wethinkcode.co.za>
2020-07-13 12:54:35 +0200

    removed int main

--- ex03/ft_is_negative.c
+++ ex03/ft_is_negative.c
@@ -4,18 +4,17 @@ void ft_putchar(int isnegresult){
 	write(1, &isnegresult, 1);
 }
 //int n = '1';
-void ft_is_negative(int n) { 
-	if (n < '0') { 
+void ft_is_negative(int n) {
+	if (n < '0') {
 		int isnegresult = 'N';
-		ft_putchar(isnegresult); 
+		ft_putchar(isnegresult);
 	} else{
-   		int isnegresult = 'P';	
-		ft_putchar(isnegresult); 
-	} 
+   		int isnegresult = 'P';
+		ft_putchar(isnegresult);
+	}
 }
 
-int main(void){
-	int n = -1;
-	ft_is_negative(n);
-}
-
+//int main(void){
+//	int n = -1;
+//	ft_is_negative(n);
+//}
